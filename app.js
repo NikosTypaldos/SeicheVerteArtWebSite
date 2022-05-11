3d27085d0621b203b411942786c7aebe3c2f60da
Nikos Typaldos <nmtypaldos@yahoo.com>
2022-05-11 14:25:45 +0300

    Moves app.js code to index.html for compatibility

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-$(function () {
-    $(document).scroll(function() {
-        var $nav = $(".navbar");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-});
-
-$(document).ready(function() {
-    $('#submitBtn').click(function(event) {  
-        alert("You will now be redirected to the mailing service");
-        window.location = "mailto:nmtypaldos@yahoo.com";
-    });
-});
+//moved code to index.html for compatibility issues with Github Pages
 
 
