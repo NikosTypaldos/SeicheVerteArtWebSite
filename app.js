12b0488a7b1407b75d950c3678b1595318d9a7eb
Nikos Typaldos <nmtypaldos@yahoo.com>
2022-05-11 19:49:46 +0300

    Fixes Javascript Compatibility

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-//moved code to index.html for compatibility issues with Github Pages
+$(function () {
+    $(document).scroll(function() {
+      var $nav = $(".navbar");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+
+$(document).ready(function() {
+    $('#submitBtn').click(function(event) {  
+        alert("You will now be redirected to the mailing service");
+        window.location = "mailto:nmtypaldos@yahoo.com";
+    });
+});
 
 
